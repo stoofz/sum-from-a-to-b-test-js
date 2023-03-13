f2235fb3d130db48b9586b3648e79ea4934ff562
Anthony Chambers <ajfchambers@gmail.com>
2023-03-13 12:42:38 -0400

    recursion added, mocha set for tests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+function sum(fromN, toN, total=0) {
+    if (fromN > toN) {
+      return total
+    }
+    total = fromN + total;
+    return sum(fromN + 1, toN, total);
+  }
 
 module.exports = sum;
